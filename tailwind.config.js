ef942ebab8edd96036313b37e6a53b1fbb7b607f
patrickcorsino <patricklcorsino@gmail.com>
2025-07-30 11:47:16 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-#
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        degen: '#00ff99',
+        darkBg: '#0a0a0a',
+        card: '#111111',
+      },
+    },
+  },
+  plugins: [],
+};
