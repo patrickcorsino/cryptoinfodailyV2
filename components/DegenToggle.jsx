b343d5723a6711a9438a216419d9350922d04dcf
patrickcorsino <patricklcorsino@gmail.com>
2025-07-30 11:48:18 -0500

    Update DegenToggle.jsx

--- components/DegenToggle.jsx
+++ components/DegenToggle.jsx
@@ -1,1 +1,20 @@
-#
+'use client';
+import { useState, useEffect } from 'react';
+
+export default function DegenToggle() {
+  const [degen, setDegen] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle('bg-black', !degen);
+    document.body.classList.toggle('bg-green-950', degen);
+  }, [degen]);
+
+  return (
+    <button
+      onClick={() => setDegen(!degen)}
+      className="p-2 px-4 rounded-full bg-degen text-black font-bold hover:scale-105 duration-200"
+    >
+      {degen ? 'DEGEN MODE ON' : 'DEGEN MODE OFF'}
+    </button>
+  );
+}
